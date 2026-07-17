b436b61385a58c407afaf93af16594b76f7a597c
Goutham <goutham.soma1@gmail.com>
2026-07-17 12:38:17 +0530

    youtube link added

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -25,7 +25,7 @@ const socialLinks = [
   },
   {
     platform: 'YouTube',
-    href: 'https://youtube.com',
+    href: 'https://youtube.com/@dharmagitagermanlanguage?si=M6D7iHxa-4QzZL6L',
     icon: (
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 24 24">
         <path d="M22.54 6.42a2.78 2.78 0 0 0-1.95-1.96C18.88 4 12 4 12 4s-6.88 0-8.59.46A2.78 2.78 0 0 0 1.46 6.42 29 29 0 0 0 1 12a29 29 0 0 0 .46 5.58 2.78 2.78 0 0 0 1.95 1.96C5.12 20 12 20 12 20s6.88 0 8.59-.46a2.78 2.78 0 0 0 1.95-1.96A29 29 0 0 0 23 12a29 29 0 0 0-.46-5.58z"/>
